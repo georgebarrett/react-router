491804923cf28ec53ed270323dce14e76871305e
georgebarrett <georgeaustinbarrett@gmail.com>
2025-11-12 17:15:12 +0000

    a Layout route has been applied to all the routes. the Layout route is nothing to do with navigation. wrapping each path/route with the same layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,19 +2,26 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Layout from "./components/Layout";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
-    },
-    {
-        path: 'about/',
-        element: <About />
-    },
-    {
-        path: '*',
-        element: <NotFound />
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: 'about',
+                element: <About />
+            },
+            {
+                path: '*',
+                element: <NotFound />
+            }
+        ]
     }
 ]);
 
