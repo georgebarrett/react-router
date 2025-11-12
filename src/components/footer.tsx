ffae3fff7b52d9b6f83528a3f3d4268e076143cb
georgebarrett <georgeaustinbarrett@gmail.com>
2025-11-12 17:50:32 +0000

    footer functional component made that will act as a footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import { SiteConfig } from "../config/index";
+
+export default function Footer() {
+    return (
+        <footer className="border-t py-6 md:py-0">
+            <div className="container mx-auto flex flex-col items-center md:h-16 md:flex-row">
+                <p className="text-center text-sm md:text-left">
+                    &copy;{' '}
+                    <Link to='/' className="underline-offset-4 hover:underline">
+                        {SiteConfig.name}
+                    </Link>
+                </p>
+            </div>
+        </footer>
+    );
+}
