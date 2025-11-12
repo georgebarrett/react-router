75cc028135db539e697209ff8725b8fc991cbad6
georgebarrett <georgeaustinbarrett@gmail.com>
2025-11-12 18:42:41 +0000

    main dashboard page component for product admin crud

---
+++ src/pages/dashboard/DashboardIndex.tsx
@@ -0,0 +1,11 @@
+export default function DashboardIndex() {
+    return (
+        <div className="space-x-12">
+            <header>
+                <h1 className="font-bold text-3xl md:text-4xl">Dashboard</h1>
+                <p className="text-lg">Store Dashboard</p>
+            </header>
+            <p>Welcome to your worst NIGHTMARE</p>
+        </div>
+    );
+}
