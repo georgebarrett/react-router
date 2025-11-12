121029e0a3e9447858c7cc488094ddc4399f15da
georgebarrett <georgeaustinbarrett@gmail.com>
2025-11-12 18:21:56 +0000

    dashboard area for the store app. this will be an admin area where i can crud products and navigate around

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,34 @@
+import { Outlet, Link } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./footer";
+
+export default function DashboardLayout() {
+    return (
+        <main className="relative flex min-h-screen flex-col">
+            <Header />
+            <section className="container mx-auto">
+                <div className="my-12 grid gap-12 md:grid-cols-[200px_1fr]">
+                    <nav className="space-y-3 md:w-[200px]">
+                        <h2 className="text-lg font-semibold ">Dashboard Menu</h2>
+                        <ul className="space-y-3">
+                            <li>
+                                <Link to='/dashboard/products' className="underline">
+                                    Products
+                                </Link>
+                            </li>
+                            <li>
+                                <Link to='/dashboard' className="underline">
+                                    Back to Dashboard
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div>
+                        <Outlet />
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </main>
+    );
+}
