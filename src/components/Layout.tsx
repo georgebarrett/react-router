e8bb5793ebce5e87c20cbef8a57c69033c3c1946
georgebarrett <georgeaustinbarrett@gmail.com>
2025-11-12 18:02:50 +0000

    the Layout of the app is taking shape. there will always be a header and a footer, which are components. the outlet contains the nested routes. when a page/url changes, the outlet fills the blank space with the right page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./footer";
 
 export default function Layout() {
     return ( 
-        <main>
-            <Outlet />
+        <main className="flex flex-col min-h-screen">
+            <Header />
+            <div className="container mx-auto py-24">
+                <Outlet />
+            </div>
+            <Footer />
         </main>
     );
 }
