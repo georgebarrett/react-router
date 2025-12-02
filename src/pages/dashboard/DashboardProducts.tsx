04f1d433d6b1c6748ac6f8043e9824138282761e
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-02 14:31:33 +0000

    the DashboardProducts function now calls the loader function so that the products are loaded onto the page. i am calling the ProductList component and passing in the products array via a prop called products. products is an object that has all the products inside it

--- src/pages/dashboard/DashboardProducts.tsx
+++ src/pages/dashboard/DashboardProducts.tsx
@@ -1,3 +1,6 @@
+import { useLoaderData } from "react-router-dom";
+import { siteConfig } from "../../config";
+import ProductList from "../../components/productsList";
 import type { Product } from "../../types";
 import { getProducts } from "../../utils/fake-api";
 
@@ -7,13 +10,15 @@ export async function loader(): Promise<{ products: Product[] }> {
 }
 
 export default function DashboardProducts() {
+    const { products } = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+
     return (
         <div className="space-y-12">
             <header>
                 <h1 className="font-bold text-3xl md:text-4xl">Products</h1>
                 <p className="text-lg">Listing of NIGHTMARES</p>
             </header>
-            <section>Coming for you soon...</section>
+            <ProductList products={products} />
         </div>
     );
 }
