981becc19bdb8fbf1ac5895308e80a084cb6220e
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-02 16:56:26 +0000

    page component to view a single product. this page will be navigated to by clicking on a product on the product list page component. dynamic routing is used. the product id is passed into the url params to load the correct product. the useLoaderData loader function ensures that single product data is loaded when the page component renders

---
+++ src/pages/dashboard/DashboardProduct.tsx
@@ -0,0 +1,54 @@
+import { ParamParseKey, Params, useLoaderData, Navigate } from "react-router-dom";
+import { siteConfig } from "../../config";
+import { Product } from "../../types";
+import { getProduct } from "../../utils/fake-api";
+
+const path = 'dashboard/products/:productId';
+
+export async function loader({
+    params: { productId },
+}: {
+    params: Params<ParamParseKey<typeof path>>;
+}): Promise<Product | null> {
+    if (productId) {
+        return getProduct(productId);
+    }
+
+    return null;
+}
+
+export default function DashboardProduct() {
+    const product = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+
+    if (!product) {
+        return <Navigate to='/dashbaord/products' replace={true} />;
+    }
+
+    return (
+        <div className="space-y-12">
+            <header className="space-y-2">
+                <h1 className="font-bold text-3xl md:text-4xl">{product.title}</h1>
+                <p className="text-lg">{product.description}</p>
+            </header>
+            <aside>
+                <img src={product.imageUrl} />
+            </aside>
+            <section>
+                <dl className="space-y-4">
+                    <div>
+                        <dt className="font-medium">brand</dt>
+                        <dd>{product.brand}</dd>
+                    </div>
+                    <div>
+                        <dt className="font-medium">category</dt>
+                        <dd>{product.category}</dd>
+                    </div>
+                    <div>
+                        <dt className="font-medium">price</dt>
+                        <dd>{product.price}</dd>
+                    </div>
+                </dl>
+            </section>
+        </div>
+    );
+}
