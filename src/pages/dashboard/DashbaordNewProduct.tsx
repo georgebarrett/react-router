207f2bc25ff3403922f69cc814e61943f431194c
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-03 18:11:11 +0000

    new dashboard component created to add new products. react routers Form element has been used. react routers Form element submits via the 'route action' function. this is different to a html form element that causes navigation to happen in the browser

---
+++ src/pages/dashboard/DashbaordNewProduct.tsx
@@ -0,0 +1,72 @@
+import { Form, redirect } from "react-router-dom";
+import { siteConfig } from "../../config";
+
+export default function DashboardNewProduct() {
+    return (
+        <div className="space-y-12">
+            <header>
+                <h1 className="font-bold text-3xl md:text-4xl">new nightmare</h1>
+                <p className="text-lg">create a new nightmare</p>
+            </header>
+            <Form method="post" className="space-y-6">
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">nightmare title</span>
+                    <input
+                        type="text"
+                        name="title"
+                        required
+                        className="border p-2 rounded"
+                    />
+                </label>
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">nightmare description</span>
+                    <textarea name="description" className="border p-2 rounded" />
+                </label>
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">price</span>
+                    <input
+                        type="text"
+                        name="price"
+                        required
+                        className="border p-2 rounded"
+                    />
+                </label>
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">brand</span>
+                    <input 
+                        type="text"
+                        name="brand"
+                        required
+                        className="border p-2 rounded"
+                    />
+                </label>
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">category</span>
+                    <input
+                        type="text"
+                        name="category"
+                        required
+                        className="border p-2 rounded"
+                    />
+                </label>
+                <label className="flex flex-col space-y-1">
+                    <span className="font-medium">Image URL</span>
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        required
+                        className="border p-2 rounded"
+                    />
+                </label>
+                <div>
+                    <button
+                        type="submit"
+                        className="bg-black hover:bg-gray-800 px-4 py-2 rounded text-white"
+                    >
+                        save
+                    </button>
+                </div>
+            </Form>
+        </div>
+    );
+}
