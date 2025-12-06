71c5fe2e9149a2fb8643d339044bfead862230e1
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-06 16:49:42 +0000

    function defined that tries to construct a new url object using js new URL and the url string passed into it. if a url can be constructed then it returns true or the error is caught and it returns false

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,11 @@ export function isValidPrice(price: string) {
     return price.match(/^\d+(\.\d{1, 2})?$/);
 }
 
+export function isValidUrl(url: string) {
+    try {
+        new URL(url);
+        return true;
+    } catch (error) {
+        return false
+    }
+}
