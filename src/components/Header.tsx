6092ec2d83847284a5668eacf683279728d624de
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-14 17:31:26 +0000

    NavLink used to create a new link to the products page for users not admins. when the user clicks the NavLinnks the isActive property will return true, which allows for different css styling compared to using normal Links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { siteConfig } from '../config/index';
 
 export default function Header() {
+    const getNavLinkClasses = ({ isActive }: { isActive: boolean }) => {
+        return isActive ? 'font-semibold' : '';
+    }
+
     return (
         <header className="sticky w-full border-b shadow-sm backdrop-blur">
-            <div className="container flex items-center justify-between h-14 mx-auto">
+            <div className="container flex items-center h-14 mx-auto">
                 <div className="flex items-center space-x-6 md:gap-10">
                     <Link to="/">
                         <h1 className="font-bold">{siteConfig.name}</h1>
                     </Link>
-                    <nav>
-                        <Link to="/about">About</Link>
+                    <nav className="flex items-center space-x-6">
+                        <NavLink to="/about" className={getNavLinkClasses}>About</NavLink>
+                        <NavLink to='/products' className={getNavLinkClasses}>Products</NavLink>
                     </nav>
-                </div>
-                <div className="ml-6">
-                    <Link to='/dashboard'>Dashboard</Link>
                 </div>
             </div>
         </header>
