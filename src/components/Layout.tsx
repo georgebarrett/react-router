4970c32eeffbc443650d472e0b039f6e0a3e686d
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-14 18:19:57 +0000

    useNavigation imported to display states when pages are loading. if a page is loading then it's opacity increases. this is to prevent user confusion and increase ux/ui experience

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 export default function Layout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === 'loading';
+
     return ( 
         <main className="flex flex-col min-h-screen">
             <Header />
-            <div className="container mx-auto py-24">
+            <div className={`container mx-auto py-24 ${
+                isLoading ? 'opacity-25' : ''
+            }`}>
                 <Outlet />
             </div>
             <Footer />
