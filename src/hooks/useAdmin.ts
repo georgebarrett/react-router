70b8c330a8ce5941db205b86c1c38654ed8acf06
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-28 14:19:03 +0000

    logic changed. three state truth model instead of two. fix: detects admin users by org:admin not just admin

--- src/hooks/useAdmin.ts
+++ src/hooks/useAdmin.ts
@@ -3,9 +3,8 @@ import { useUser } from "@clerk/clerk-react";
 export default function useAdmin() {
     const { isLoaded, isSignedIn, user } = useUser();
 
-    if (!isLoaded || !isSignedIn) {
-        return null;
-    }
+    if (!isLoaded) return null;
+    if (!isSignedIn || !user) return false;
 
-    return user.organizationMemberships.some((item) => item.role === 'admin') ?? false;
+    return user.organizationMemberships?.some((item) => item.role === 'org:admin') ?? false;
 }
