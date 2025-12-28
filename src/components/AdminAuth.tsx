ea722b67ef81b333ed23d479571a6112d1566948
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-28 14:12:49 +0000

    spinner imported for loading times. logic changed to prevent instant navigation to a 404. admin status has to be false to navigate to a 404

--- src/components/AdminAuth.tsx
+++ src/components/AdminAuth.tsx
@@ -1,16 +1,19 @@
 import { useEffect, type ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
+import Spinner from "./Spinner";
 
 export default function AdminAuth({children}: { children: ReactNode }) {
     const navigate = useNavigate();
     const isAdmin = useAdmin();
 
     useEffect(() => {
-        if (!isAdmin) {
+        if (isAdmin === false) {
             navigate(`/404`);
         }
     }, [isAdmin, navigate]);
+
+    if (isAdmin === null) return <Spinner />;
 
     return children;
 }
