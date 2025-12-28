0e9c4fe61f8f98d734dd573e2d0f75e1ee5f7b89
georgebarrett <georgeaustinbarrett@gmail.com>
2025-12-28 12:08:53 +0000

    import amended

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import Header from "./Header";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 export default function DashboardLayout() {
     return (
