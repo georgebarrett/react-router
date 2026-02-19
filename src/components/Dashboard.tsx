081d04a4c93ee94f2a0ffc843702590d8a3fbf16
georgebarrett <georgeaustinbarrett@gmail.com>
2026-02-19 17:40:38 +0000

    side bar removed from Dashboard as it was duplicating what was on the navbar and therefore not needed

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,34 +1,15 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 export default function DashboardLayout() {
-    return (
-        <main className="relative flex min-h-screen flex-col">
-            <Header />
-            <section className="container mx-auto">
-                <div className="my-12 grid gap-12 md:grid-cols-[200px_1fr]">
-                    <nav className="space-y-3 md:w-[200px]">
-                        <h2 className="text-lg font-semibold ">Dashboard Menu</h2>
-                        <ul className="space-y-3">
-                            <li>
-                                <Link to='/dashboard/products' className="underline">
-                                    Products
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to='/dashboard' className="underline">
-                                    Back to Dashboard
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div>
-                        <Outlet />
-                    </div>
-                </div>
-            </section>
-            <Footer />
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col">
+      <Header />
+      <section className="max-w-6xl mx-auto px-6 py-24 w-full">
+        <Outlet />
+      </section>
+      <Footer />
+    </main>
+  );
 }
